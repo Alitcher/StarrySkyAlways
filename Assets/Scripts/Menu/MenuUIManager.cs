9972f3f2fc59174b731c565fe776fb64f49b383d
Alicia Sudlerd <alicia22nzd@gmail.com>
2021-01-30 03:00:32 +0700

    project - implement menu scene

---
+++ Assets/Scripts/Menu/MenuUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject m_PlayButton, m_CreditsButton, m_QuitButton;
+    [SerializeField] private GameObject m_CreditPanel;
+
+
+    private void Awake()
+    {
+        m_CreditPanel.SetActive(false);
+    }
+
+    public void OnClickPlayButton()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+
+    public void ActiveCreditPanel(bool isActive)
+    {
+        m_CreditPanel.SetActive(isActive);
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+}
